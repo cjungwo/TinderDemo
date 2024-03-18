63c87a96646484823a8114810f152671902cff17
Cjungwo <jwoo516@naver.com>
2024-03-18 13:03:22 +1100

    Initial Commit

---
+++ TinderDemo/TinderDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderDemoApp.swift
+//  TinderDemo
+//
+//  Created by JungWoo Choi on 18/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct TinderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
